54a9783d70514cbf5d8b80802ea0218fc21515c9
Kevin Blanchard <blanchardkevinj@gmail.com>
2024-03-02 15:37:08 -0500

    root site

--- scripts/enable_sites.sh
+++ scripts/enable_sites.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 # Enable all the sites that we want to work.
-filenames='sgdiscord_supergoon_com.conf wedding_supergoon_com.conf'
+filenames='sgdiscord_supergoon_com.conf wedding_supergoon_com.conf root_supergoon_com.conf'
 for name in $filenames; do
     ln -s "/etc/nginx/sites_available/$name" "/etc/nginx/sites_enabled/$name"
 done
